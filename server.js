a7c34ce39171fc86208c01a965874c32e38ff16d
Derek Chan <derekchan@Chan-Derek.local>
2017-04-02 22:40:58 -0400

    Add production server.

--- server.js
+++ server.js
@@ -2,21 +2,32 @@ const express = require('express');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpack = require('webpack');
 const webpackConfig = require('./webpack.config.js');
+
+const isDeveloping = process.env.NODE_ENV !== 'production';
 const app = express();
 
-const compiler = webpack(webpackConfig);
 
-app.use(express.static(__dirname));
+if (isDeveloping) {
+  const compiler = webpack(webpackConfig);
 
-app.use(webpackDevMiddleware(compiler, {
-  hot: false,
-  filename: 'react-bundle.js',
-  publicPath: '/',
-  stats: {
-    colors: true,
-  },
-  historyApiFallback: true,
-}));
+  app.use(express.static(__dirname));
+
+  app.use(webpackDevMiddleware(compiler, {
+    hot: false,
+    filename: 'react-bundle.js',
+    publicPath: '/',
+    stats: {
+      colors: true,
+    },
+    historyApiFallback: true,
+  }));
+} else {
+  app.use(express.static(__dirname));
+  app.get('*', function response(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
+}
+
 
 const server = app.listen(8080, function() {
   const host = server.address().address;
